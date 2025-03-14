fd532d15d98d2462a3173c0ccecd6ebc8824a1f3
silvinor <366673+silvinor@users.noreply.github.com>
2025-03-14 18:08:15 +1100

    Add BNR1

---
+++ keyboards/binepad/bnr1/keymaps/binepad/rules.mk
@@ -0,0 +1,13 @@
+# Copyright 2022 Binepad (@binpad)
+# SPDX-License-Identifier: GPL-2.0-or-later
+
+NKRO_ENABLE = no  # too few keys for NKRO to make sense
+VIA_ENABLE = yes
+VIAL_ENABLE = yes
+QMK_SETTINGS = no  # don't need these
+
+ENCODER_MAP_ENABLE = yes
+MIDI_ENABLE = yes
+
+VPATH += $(QMK_USERSPACE)/keyboards/binepad/common
+SRC += binepad_common.c
