6008c9c98b2e18ae02888d6e2aba54d95b4b19df
silvinor <366673+silvinor@users.noreply.github.com>
2025-05-12 15:38:46 +1000

    .

--- keyboards/binepad/bnk8/keymaps/binepad/config.h
+++ keyboards/binepad/bnk8/keymaps/binepad/config.h
@@ -8,9 +8,9 @@
 
 #define DYNAMIC_KEYMAP_LAYER_COUNT 8
 
-#define USER_SET_KEYCODE_SEND_VERSION QK_KB_9
-
 /* Community modules */
+
+#define USER_SET_KEYCODE_SEND_VERSION QK_KB_9
 
 #define DOUBLE_TAP_KEY_COUNT 2
 
