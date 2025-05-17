6f68c705d94aa04959d66ea8fee9547399e7aae2
silvinor <366673+silvinor@users.noreply.github.com>
2025-05-17 19:28:46 +1000

    kn01

--- keyboards/binepad/kn01/keymaps/binepad/keymap.c
+++ keyboards/binepad/kn01/keymaps/binepad/keymap.c
@@ -3,37 +3,21 @@
 
 #include QMK_KEYBOARD_H
 
-#include "binepad_common.h"
-
 #ifndef VIAL_PROTOCOL_VERSION
-#    error "This keymap is only intended for VIAL. Please use VIAL-QMK."
+#    error "This keymap is intended for VIAL. Please use VIAL-QMK."
 #endif
 
-// clang-format off
-const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
-    [0] = LAYOUT_ortho_1x1(
-        LT(1, KC_MUTE)
-    ),
-
-    [1] = LAYOUT_ortho_1x1(
-        _______
-    )
-};
-// clang-format on
-
-#if defined(ENCODER_MAP_ENABLE)
-// clang-format off
-const uint16_t PROGMEM encoder_map[][NUM_ENCODERS][2] = {
-    [0] = { ENCODER_CCW_CW(KC_VOLD, KC_VOLU) },
-    [1] = { ENCODER_CCW_CW(KC_MS_WH_DOWN, KC_MS_WH_UP) }
-};
-// clang-format on
+#ifdef COMMUNITY_MODULE_SR_VERSION_ENABLE
+#    include "sr_version.h"
 #endif
 
 bool process_record_user(uint16_t keycode, keyrecord_t *record) {
-    // call into binepad_common.c
-    if (!process_record_binepad(keycode, record)) {
-        return false;
+    switch (keycode) {
+#ifdef COMMUNITY_MODULE_SR_VERSION_ENABLE
+        case USER_SET_KEYCODE_SEND_VERSION: {
+            return process_keycode_sr_version(record);
+        }
+#endif
     }
     return true;
 }
