a17b48aa2fffdf673c1e16b612c194a7fa1a9af2
silvinor <366673+silvinor@users.noreply.github.com>
2025-05-17 17:54:25 +1000

    bn006

--- keyboards/binepad/bn006/keymaps/binepad/keymap.c
+++ keyboards/binepad/bn006/keymaps/binepad/keymap.c
@@ -3,34 +3,21 @@
 
 #include QMK_KEYBOARD_H
 
-#include "binepad_common.h"
-
 #ifndef VIAL_PROTOCOL_VERSION
-#    error "This keymap is only intended for VIAL. Please use VIAL-QMK."
+#    error "This keymap is intended for VIAL. Please use VIAL-QMK."
 #endif
 
-// clang-format off
-const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
-    /*
-     * ┌───┬───┬───┐
-     * │ A │ B │ C │
-     * ├───┼───┼───┤
-     * │ D │ E │ F │
-     * └───┴───┴───┘
-     */
-    [0] = LAYOUT_ortho_2x3(
-        KC_MPLY, KC_MUTE, KC_VOLU,
-        KC_MPRV, KC_MNXT, KC_VOLD
-    )
-
-    // The rest defaults to `_______` for each key
-};
-// clang-format on
+#ifdef COMMUNITY_MODULE_SR_VERSION_ENABLE
+#    include "sr_version.h"
+#endif
 
 bool process_record_user(uint16_t keycode, keyrecord_t *record) {
-    // call into binepad_common.c
-    if (!process_record_binepad(keycode, record)) {
-        return false;
+    switch (keycode) {
+#ifdef COMMUNITY_MODULE_SR_VERSION_ENABLE
+        case USER_SET_KEYCODE_SEND_VERSION: {
+            return process_keycode_sr_version(record);
+        }
+#endif
     }
     return true;
 }
--- keyboards/binepad/bn006/keymaps/binepad/rules.mk
+++ keyboards/binepad/bn006/keymaps/binepad/rules.mk
@@ -5,7 +5,5 @@ NKRO_ENABLE = no  # too few keys for NKRO to make sense
 VIA_ENABLE = yes
 VIAL_ENABLE = yes
 VIALRGB_ENABLE = yes
-QMK_SETTINGS = no  # don't need these
 
-VPATH += $(QMK_USERSPACE)/keyboards/binepad/common
-SRC += binepad_common.c
+QMK_SETTINGS = no  # don't need these
