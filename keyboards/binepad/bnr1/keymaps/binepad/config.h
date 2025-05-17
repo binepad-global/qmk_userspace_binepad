dd8a9dccc14d9e9a3d4badbbb9b038931b8b0b1d
silvinor <366673+silvinor@users.noreply.github.com>
2025-05-17 18:31:06 +1000

    bnr1

--- keyboards/binepad/bnr1/keymaps/binepad/config.h
+++ keyboards/binepad/bnr1/keymaps/binepad/config.h
@@ -3,10 +3,11 @@
 
 #pragma once
 
-#define TAPPING_TERM 175
+#define TAPPING_TERM 250
+#define RETRO_TAPPING
 
 #ifdef DYNAMIC_KEYMAP_LAYER_COUNT
-    #undef DYNAMIC_KEYMAP_LAYER_COUNT
+#    undef DYNAMIC_KEYMAP_LAYER_COUNT
 #endif
 #define DYNAMIC_KEYMAP_LAYER_COUNT 2
 
@@ -14,5 +15,10 @@
 
 #define VIAL_KEYBOARD_UID {0xFB, 0xB1, 0x1A, 0xCD, 0x1F, 0x3A, 0x54, 0x28}
 
-#define VIAL_UNLOCK_COMBO_ROWS { 0 }
-#define VIAL_UNLOCK_COMBO_COLS { 0 }
+#define VIAL_UNLOCK_COMBO_ROWS {0}
+#define VIAL_UNLOCK_COMBO_COLS {0}
+
+#ifdef MANUFACTURER
+#    undef MANUFACTURER
+#    define MANUFACTURER ""
+#endif
--- keyboards/binepad/bnr1/keymaps/binepad/rules.mk
+++ keyboards/binepad/bnr1/keymaps/binepad/rules.mk
@@ -2,4 +2,21 @@
 # SPDX-License-Identifier: GPL-2.0-or-later
 
 VIAL_ENABLE = yes
-QMK_SETTINGS = no  # don't need these
+
+ifneq ($(findstring v1,$(KEYBOARD)),)
+    QMK_SETTINGS = no
+    LTO_ENABLE = yes
+endif
+# ifneq ($(findstring v2,$(KEYBOARD)),)
+#     QMK_SETTINGS = yes
+# endif
+ifneq ($(findstring v3,$(KEYBOARD)),)
+    $(info Ψ Vial-QMK qmk_settings.c hack applied)
+    QMK_SETTINGS = no
+    AUTO_SHIFT_ENABLE := yes
+    SRC += byo_settings.c
+    OPT_DEFS += -DQMK_SETTINGS \
+        -DAUTO_SHIFT_NO_SETUP -DAUTO_SHIFT_REPEAT_PER_KEY -DAUTO_SHIFT_NO_AUTO_REPEAT_PER_KEY \
+        -DPERMISSIVE_HOLD_PER_KEY -DHOLD_ON_OTHER_KEY_PRESS_PER_KEY -DQUICK_TAP_TERM_PER_KEY -DRETRO_TAPPING_PER_KEY \
+        -DCOMBO_TERM_PER_COMBO
+endif
