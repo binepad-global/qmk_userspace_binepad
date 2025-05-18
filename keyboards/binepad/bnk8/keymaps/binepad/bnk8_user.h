966ee9a74c7752b9f7fd5f1cf8777ae209eb644b
silvinor <366673+silvinor@users.noreply.github.com>
2025-05-18 12:42:05 +1000

    bnk8

---
+++ keyboards/binepad/bnk8/keymaps/binepad/bnk8_user.h
@@ -0,0 +1,69 @@
+// Copyright 2024 binepad (@binepad)
+// SPDX-License-Identifier: GPL-2.0-or-later
+
+#pragma once
+
+#include "quantum.h"
+#ifdef VIA_ENABLE // Only works if VIA is enabled
+#    include "color.h"
+
+#    if !defined(VIA_EEPROM_CUSTOM_CONFIG_SIZE) || VIA_EEPROM_CUSTOM_CONFIG_SIZE != 34
+#        error "Please set VIA_EEPROM_CUSTOM_CONFIG_SIZE to 34 in config.h"
+#    endif
+
+// clang-format off
+
+enum via_per_key_value {
+    id_custom_color = 1,
+    id_custom_lyrclr,      // 2
+    id_custom_l0_off,      // 3
+    id_encoder_resolution, // 4
+    id_firmware_button = 99
+};
+
+enum via_id_firmware_button {
+    id_buton_ignore = 0,
+    id_button_bootloader,   // 1
+    id_button_reboot,       // 2
+    id_button_debug_toggle, // 3
+    id_button_clear_eeprom  // 4
+};
+
+enum custom_keycodes {
+    B8_CAFFEINE_TOGGLE = QK_KB_0,
+    B8_MUTE_PLAY_DOUBLE,    // 1
+    B8_LAYER_JUMP,          // 2
+    B8_LAYER_UP,            // 3
+    B8_LAYER_DOWN,          // 4
+    B8_LAYER_PRINT,         // 5
+    B8_CAFFEINE_ON,         // 6
+    B8_CAFFEINE_OFF,        // 7
+    B8__SPARE__8,           // 8
+    // USER_SET_KEYCODE_SEND_VERSION
+};
+
+// clang-format on
+
+typedef struct PACKED {
+    uint8_t h;
+    uint8_t s;
+} hs_t;
+
+typedef union {
+    uint8_t raw[16];
+    struct {
+        hs_t color[8];
+        hs_t lyrclr[8];
+    };
+    union {
+        uint8_t flags;
+        struct {
+            bool l0_off : 1;
+        };
+    };
+    uint8_t enc_res;
+} user_config_t;
+
+extern user_config_t g_user_config;
+
+#endif // VIA_ENABLE
--- keyboards/binepad/bnk8/keymaps/binepad/rules.mk
+++ keyboards/binepad/bnk8/keymaps/binepad/rules.mk
@@ -1,24 +1,8 @@
 # Copyright 2024 Binepad (@binpad)
 # SPDX-License-Identifier: GPL-2.0-or-later
 
-VIA_ENABLE = yes
-ENCODER_MAP_ENABLE = yes
-
-# CONSOLE_ENABLE = yes  ## for debugging!
-# COMMAND_ENABLE = yes
-
 RGB_MATRIX_CUSTOM_KB = yes
-CAFFEINE_ENABLE = yes
 
-OS_DETECTION_ENABLE = yes
-ENCODER_DRIVER ?= custom  # use custom encoder
+OS_DETECTION_ENABLE = yes # needed for BP Encoder Configurable community module
 
-VPATH += $(QMK_USERSPACE)/keyboards/binepad/common
-SRC += encoder_configurable.c  # our custom encoder
-SRC += binepad_common.c
-SRC += bnk8_effect.c
-
-ifeq ($(strip $(CAFFEINE_ENABLE)), yes)
-    SRC += caffeine.c
-    OPT_DEFS += -DCAFFEINE_ENABLE
-endif
+SRC += bnk8_via_config.c
