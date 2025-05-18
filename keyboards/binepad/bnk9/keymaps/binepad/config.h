d61e56799d63924fd462043e9642f53a92efab98
silvinor <366673+silvinor@users.noreply.github.com>
2025-05-18 12:43:37 +1000

    bnk9

--- keyboards/binepad/bnk9/keymaps/binepad/config.h
+++ keyboards/binepad/bnk9/keymaps/binepad/config.h
@@ -3,13 +3,23 @@
 
 #pragma once
 
-#define MIDI_ADVANCED
-
-#define VIA_EEPROM_CUSTOM_CONFIG_SIZE 18
+#define VIA_EEPROM_CUSTOM_CONFIG_SIZE 38 // needed for custom save to work, calc. from user_config_t
+#define VIA_FIRMWARE_VERSION 2           // needed to load next version of via json
 
 #define DYNAMIC_KEYMAP_LAYER_COUNT 9
 
-#ifdef OS_DETECTION_ENABLE
-    // res at 2 does not work so well on macOS, each click moves by 2
-#    define ENCODER_RESOLUTION_MACOS 4
+#define MIDI_ADVANCED // wanted by some customers
+
+/* Community modules */
+
+#define USER_SET_KEYCODE_SEND_VERSION QK_KB_9
+
+#define DOUBLE_TAP_KEY_COUNT 2
+
+// The below overrides the QMK repo keyboard.json sets
+#if defined(ENCODER_RESOLUTION) && defined(OS_DETECTION_ENABLE)
+#    define ENCODER_RESOLUTION_MACOS 4 // Used by BP_Encoder_Config community module
+// #    if ENCODER_RESOLUTION == 4
+// #        define ENCODER_DEFAULT_POS 0x3
+// #    endif
 #endif
