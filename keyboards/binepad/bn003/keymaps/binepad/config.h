9811847c0b896a6a175adbf39a71ba8c0c9bde70
silvinor <366673+silvinor@users.noreply.github.com>
2025-05-18 12:39:02 +1000

    bn003

---
+++ keyboards/binepad/bn003/keymaps/binepad/config.h
@@ -0,0 +1,14 @@
+// Copyright 2024 binepad (@binepad)
+// SPDX-License-Identifier: GPL-2.0-or-later
+
+#pragma once
+
+#define VIAL_KEYBOARD_UID {0x52, 0x60, 0xE3, 0x33, 0xC2, 0xFF, 0x3A, 0xCC}
+
+#define VIAL_UNLOCK_COMBO_ROWS { 0, 0 }
+#define VIAL_UNLOCK_COMBO_COLS { 0, 2 }
+
+#define VIAL_TAP_DANCE_ENTRIES 3
+#define DYNAMIC_KEYMAP_LAYER_COUNT 3
+
+#define USER_SET_KEYCODE_SEND_VERSION QK_KB_9
--- keyboards/binepad/bn003/keymaps/binepad/rules.mk
+++ keyboards/binepad/bn003/keymaps/binepad/rules.mk
@@ -3,6 +3,9 @@
 
 NKRO_ENABLE = no  # too few keys for NKRO to make sense
 VIA_ENABLE = yes
+VIAL_ENABLE = yes
+LTO_ENABLE = yes
 
-VPATH += $(QMK_USERSPACE)/keyboards/binepad/common
-SRC += binepad_common.c
+QMK_SETTINGS = no
+COMBO_ENABLE = no
+KEY_OVERRIDE_ENABLE = no
