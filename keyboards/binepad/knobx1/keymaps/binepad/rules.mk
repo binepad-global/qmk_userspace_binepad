e94d2099516199e6e2a290f0359bc2acbd0f0ecb
silvinor <366673+silvinor@users.noreply.github.com>
2025-05-18 12:45:47 +1000

    knobx1

---
+++ keyboards/binepad/knobx1/keymaps/binepad/rules.mk
@@ -0,0 +1,10 @@
+# Copyright 2025 Binepad (@binepad)
+# SPDX-License-Identifier: GPL-2.0-or-later
+
+OS_DETECTION_ENABLE = yes # needed for BP Encoder Configurable community module
+
+# VIA_ENABLE = yes         # set in json
+# ENCODER_MAP_ENABLE = yes # set in json
+OS_DETECTION_ENABLE = yes
+MIDI_ENABLE = yes
+VIAL_ENABLE = yes
